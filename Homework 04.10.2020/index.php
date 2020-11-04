9f9fdcda3933aa29ddd7c1336aeebee76fe9e8ad
PanasevichZhenya <73422963+PanasevichZhenya@users.noreply.github.com>
2020-11-04 14:51:32 +0300

    Create index.php

---
+++ Homework 04.10.2020/index.php
@@ -0,0 +1,20 @@
+<form action = 'handler.php' method="post">
+
+    <h3>Удалить лишние пробелы в тексте : <br></h3>
+       <input type="text" name="withoutSpaces" ><br>
+
+    <h3> Поиск слов, которые не меньше 3-ех, но не больше 5-ти символов :<br></h3>
+       <input type="text" name="wordSize" ><br>
+
+    <h3>Определить, является ли строка палиндромом :<br></h3>
+       <input type="text" name="palindrom" ><br>
+
+    <h3>Найти самое длинное слово :<br></h3>
+       <input type="text" name="longestWord" ><br>
+
+    <h3>Количество строчных и прописных букв в строке:<br></h3>
+       <input type="text" name="lowUpCase" ><br><br>
+
+    <button type="submit">Отправить</button>
+
+</form>
