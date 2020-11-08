5f61be5dcd751dcdcd70afe09539219b9f6f6a66
PanasevichZhenya <73422963+PanasevichZhenya@users.noreply.github.com>
2020-11-08 15:12:43 +0300

    Update index.php

--- Homework 04.10.2020/index.php
+++ Homework 04.10.2020/index.php
@@ -1,20 +1,11 @@
-<form action = 'handler.php' method="post">
-
-    <h3>Удалить лишние пробелы в тексте : <br></h3>
-       <input type="text" name="withoutSpaces" ><br>
-
-    <h3> Поиск слов, которые не меньше 3-ех, но не больше 5-ти символов :<br></h3>
-       <input type="text" name="wordSize" ><br>
-
-    <h3>Определить, является ли строка палиндромом :<br></h3>
-       <input type="text" name="palindrom" ><br>
-
-    <h3>Найти самое длинное слово :<br></h3>
-       <input type="text" name="longestWord" ><br>
-
-    <h3>Количество строчных и прописных букв в строке:<br></h3>
-       <input type="text" name="lowUpCase" ><br><br>
-
-    <button type="submit">Отправить</button>
-
+<form action="handler.php" method="post">
+    <input type="text" name="string">
+    <select name="funcName" id="">
+        <option value="withoutSpaces">Функция 1</option>
+        <option value="wordSize">Функция 2</option>
+        <option value="palindrom">Функция 3</option>
+        <option value="longestWord">Функция 4</option>
+        <option value="lowUpCase">Функция 5</option>
+    </select>
+    <input type="submit" value="send">
 </form>
