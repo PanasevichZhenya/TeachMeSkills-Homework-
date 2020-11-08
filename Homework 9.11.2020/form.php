d7e2bb403ff37cd690036e8c341b427e9ad2c7dd
Eugenij Panasevich <panasevichzhenya@gmail.com>
2020-11-08 18:56:32 +0300

    create Homework

---
+++ Homework 9.11.2020/form.php
@@ -0,0 +1,11 @@
+<form action="handler.php" method="post">
+    <input type="text" name="string">
+    <select name="funcName" id="">
+        <option value="withoutSpaces">Функция 1</option>
+        <option value="wordSize">Функция 2</option>
+        <option value="palindrom">Функция 3</option>
+        <option value="longestWord">Функция 4</option>
+        <option value="lowUpCase">Функция 5</option>
+    </select>
+    <input type="submit" value="send">
+</form>-->
